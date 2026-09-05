c20c67e4f8270792d364763ea7900a9e8ec09586
Transport for Somerset <141499229+transportforsomerset@users.noreply.github.com>
2026-09-05 23:42:35 +0100

    Update get-falcon-journeys.ts

--- scripts/get-falcon-journeys.ts
+++ scripts/get-falcon-journeys.ts
@@ -1,16 +1,12 @@
-console.log("I'll try to get the Journey Data from those sneaky devs at MegaBus!");
-
 const url = "https://megabus.tmpanel.co.uk/Tracker/GetJourney";
 
 const body = new URLSearchParams({
   FromStage: "",
   ToStage: "",
-  RouteID: "FALC",
+  RouteID: "57",
   TicketNumber: "",
-  IsStageSelection: "0",
+  IsStageSelection: "false",
 });
-
-console.log("I'll try to get the Journey Data from those sneaky devs at MegaBus!");
 
 const response = await fetch(url, {
   method: "POST",
@@ -25,4 +21,4 @@ console.log(`HTTP status: ${response.status}`);
 const text = await response.text();
 
 console.log(`Response length: ${text.length}`);
-console.log(text.slice(0, 500));
+console.log(text.slice(0, 1000));
