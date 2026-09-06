07ba84ecd91160a7beebd9e1cb6e04f519de2c39
Transport for Somerset <141499229+transportforsomerset@users.noreply.github.com>
2026-09-06 22:38:21 +0100

    Update fetch-flix.ts

--- scripts/fetch-flix.ts
+++ scripts/fetch-flix.ts
@@ -1,1 +1,9 @@
+const debug = process.argv.includes("--debug");
+
 console.log("Flix isn't LIVE yet!");
+
+if (debug) {
+  console.log("--debug: true");
+} else {
+  console.log("--debug: not found");
+}
