15bd174c8c11b3a88b4c5cc8ace34d9506edeaea
Transport for Somerset <141499229+transportforsomerset@users.noreply.github.com>
2026-09-08 22:39:34 +0100

    Update BODS-zip.ts

--- scripts/BODS-zip.ts
+++ scripts/BODS-zip.ts
@@ -142,7 +142,7 @@ console.log("Mapping VehicleActivity records to Vehicle[]...");
 
 const mapStart = performance.now();
 
-const vehicles: Vehicle = [];
+const vehicles: Vehicle[] = [];
 
 for (const activity of vehicleActivities) {
   const journey = activity.MonitoredVehicleJourney;
