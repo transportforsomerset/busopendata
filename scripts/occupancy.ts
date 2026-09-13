7343a6dfb7db180d252a47987f804135e8c38cf4
Transport for Somerset <141499229+transportforsomerset@users.noreply.github.com>
2026-09-13 22:17:49 +0100

    Create occupancy.ts

---
+++ scripts/occupancy.ts
@@ -0,0 +1,29 @@
+function encodeOccupancy(occupancy: string): number {
+  switch (occupancy) {
+    case "":
+      return 0;
+    case "seatsAvailable":
+      return 1;
+    case "standingAvailable":
+      return 2;
+    case "full":
+      return 3;
+    default:
+      throw new Error(`Unknown occupancy value: ${occupancy}`);
+  }
+}
+
+function decodeOccupancy(value: number): string {
+  switch (value) {
+    case 0:
+      return "";
+    case 1:
+      return "seatsAvailable";
+    case 2:
+      return "standingAvailable";
+    case 3:
+      return "full";
+    default:
+      throw new Error(`Unknown occupancy code: ${value}`);
+  }
+}
