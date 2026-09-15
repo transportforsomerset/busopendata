613cc5c41d13dcbeec6daa167b77ff3c80a2f0fd
Transport for Somerset <141499229+transportforsomerset@users.noreply.github.com>
2026-09-15 14:49:56 +0100

    Update create-dictionary.ts

--- scripts/create-dictionary.ts
+++ scripts/create-dictionary.ts
@@ -5,25 +5,26 @@ export interface Dictionary<T extends string = string> {
 
 export function createDictionary<T extends string>(
   values: T[],
-  initialValue?: T
+  initialValue?: T,
+  sort = false
 ): Dictionary<T> {
   const dictionary: T[] = [];
   const indexByValue = new Map<T, number>();
 
   if (initialValue !== undefined) {
     dictionary.push(initialValue);
-    indexByValue.set(initialValue, 0);
+    //indexByValue.set(initialValue, 0);
   }
 
   for (const value of values) {
     if (!indexByValue.has(value)) {
-      indexByValue.set(value, dictionary.length);
+      //indexByValue.set(value, dictionary.length);
       dictionary.push(value);
     }
   }
 
-  return {
-    values: dictionary,
-    indexByValue,
-  };
+  if (sort) { dictionary.sort((a, b) => a.localeCompare(b)); }
+  dictionary.forEach((value, index) => { indexByValue.set(value, index); });
+
+  return { values: dictionary, indexByValue, };
 }
